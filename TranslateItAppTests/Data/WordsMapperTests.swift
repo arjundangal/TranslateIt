9c7310976d6958c085a3b4033b1ae11512dd4983
Arjun Dangal <arjundangal4@gmail.com>
2022-07-23 18:11:23 +0545

    Ensure that WordsMapper maps data correctly

---
+++ TranslateItAppTests/Data/WordsMapperTests.swift
@@ -0,0 +1,39 @@
+//
+//  WordsMapperTests.swift
+//  TranslateItAppTests
+//
+//  Created by Arjun Dangal on 23/7/2022.
+//
+
+import XCTest
+@testable import TranslateItApp
+
+class WordsMapperTests: XCTestCase {
+    
+    func test_wordsMapper_mapsCorrectly_whenCorrectJsonIsProvided() {
+        let item1 = makeData(english: "English", spanish: "Espanyol")
+        let item2 = makeData(english: "Translate", spanish: "SpanishTranslate")
+        
+        let result = try! WordsMapper.map(data: makeItemsJSON([item1.json, item2.json]))
+        
+        XCTAssertEqual(result, [item1.item, item2.item])
+        
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeData(english: String,
+                          spanish: String) -> (item: WordPair, json: [String: Any]) {
+        let item = WordPair(originalWord: english, translatedWord: spanish)
+        let data = [
+            "text_eng": english,
+            "text_spa": spanish
+        ]
+        return (item,data)
+     }
+    
+    func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: items)
+    }
+    
+}
