b156a6d0f9e13995134ea48b5a7181c743601d7f
Arjun Dangal <arjundangal4@gmail.com>
2022-07-25 22:21:25 +0545

    Write test to ensure window is set properly

--- TranslateItAppTests/Main/SceneDelegateTests.swift
+++ TranslateItAppTests/Main/SceneDelegateTests.swift
@@ -10,5 +10,24 @@ import XCTest
 
 class SceneDelegateTests:  XCTestCase {
     
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindowSpy()
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+    }
+    
+   
+    
+    private class UIWindowSpy: UIWindow {
+        var makeKeyAndVisibleCallCount = 0
+        
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCallCount = 1
+        }
+    }
  }
 
