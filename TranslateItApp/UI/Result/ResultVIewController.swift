a8c439ac9bf32500bf8ef33d41fd0210be4f95f9
Arjun Dangal <arjundangal4@gmail.com>
2022-07-25 17:37:58 +0545

    Create ResultViewController

---
+++ TranslateItApp/UI/Result/ResultVIewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultVIewController.swift
+//  TranslateItApp
+//
+//  Created by Arjun Dangal on 25/7/2022.
+//
+
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
